154e839d44f486b804988307829aaadc84011f81
jsapro <77.3.77@mail.ru>
2023-09-21 13:42:24 +0300

    add dropdown to choose class auto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,69 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cost-of-car-rental-calc-angular';
+
+  selectedClass: string = '--Выберите класс авто--';
+  models: Array<any> = [];
+
+  cars: Array<any> = [
+    {
+      name: 'B',
+      models: [
+        {
+          name: 'BMW X5',
+          rentCostForDay: {
+            shortRent: 10000,
+            averageRent: 8000,
+            longRent: 5000,
+          },
+        },
+
+        {
+          name: 'Nissan Quashkai',
+          rentCostForDay: {
+            shortRent: 9000,
+            averageRent: 7000,
+            longRent: 4500,
+          },
+        },
+        {
+          name: 'Ford Focus',
+          rentCostForDay: {
+            shortRent: 8000,
+            averageRent: 6500,
+            longRent: 4000,
+          },
+        },
+      ],
+    },
+
+    {
+      name: 'C',
+      models: [
+        {
+          name: 'Ford Transit',
+          rentCostForDay: {
+            shortRent: 12000,
+            averageRent: 10000,
+            longRent: 8000,
+          },
+        },
+
+        {
+          name: 'Mercedes-Benz Sprinter',
+          rentCostForDay: {
+            shortRent: 13000,
+            averageRent: 11000,
+            longRent: 9000,
+          },
+        },
+      ],
+    },
+  ];
+
+  changeClass(car: any) {
+    this.models = this.cars.find(
+      (_car: any) => _car.name == car.target.value
+      ).models;
+  }
 }
